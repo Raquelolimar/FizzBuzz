384037ae610f16d7aee8b79fce44efd0fc1e76e5
Raquelolimar <raquelolivenza@gmail.com>
2025-04-05 23:32:58 +0200

    sabado

--- ejercis.py
+++ ejercis.py
@@ -1,10 +1,10 @@
 def fizz_buzz (num):
     for num in range (1,101):
-        if num 3% ==0 and num 5% ==0:
+        if num  % 3 ==0 and num % 5 ==0:
             print('FizzBuzz')
-        elif num 3% ==0:
+        elif num % 3 ==0:
          print('Fizz')
-        elif num 5% ==0:
+        elif num % 5 ==0:
             print('Buzz')
         else:
             print(num)
